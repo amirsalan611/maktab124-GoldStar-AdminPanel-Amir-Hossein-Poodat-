086629653b2b3d11469548d8cc72dd2be9b70118
arsalan poodat <amir.arsalan.poudat@gmail.com>
2025-04-11 12:41:41 +0330

    feat: handel get admin bio for header

---
+++ src/services/auth/getAdminBio/getAdminBio.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { API_KEY, BASE_URL } from "../../API/API";
+
+async function getAdminBio() {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(`${BASE_URL}/api/users/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        api_key: `${API_KEY}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default getAdminBio;
