2047c512978fb0249b5e8b721ecc32a23ce5da0d
arsalan poodat <amir.arsalan.poudat@gmail.com>
2025-04-11 20:24:53 +0330

    feat: create the dynamic table

--- src/components/shared/Table/Table.tsx
+++ src/components/shared/Table/Table.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 
+interface Column {
+  key: string;
+  label: string;
+}
+
 interface TableProps {
   data: any[];
-  columns: string[];
+  columns: Column[];
 }
 
 const Table: React.FC<TableProps> = ({ data, columns }) => {
+  console.log(data)
   return (
-    <div className="overflow-x-auto">
-      <table className="min-w-full table-auto border-collapse">
-        <thead>
+    <div className="overflow-y-auto">
+      <table className="table-auto border-collapse w-full border rounded-xl shadow-sm">
+        <thead className="">
           <tr>
             {columns.map((column, index) => (
-              <th key={index} className="px-4 py-2 border-b text-left">
-                {column}
+              <th key={index} className="px-4 py-2 border-b border-x text-center">
+                {column.label}
               </th>
             ))}
           </tr>
@@ -21,16 +27,28 @@ const Table: React.FC<TableProps> = ({ data, columns }) => {
         <tbody>
           {data.length === 0 ? (
             <tr>
-              <td colSpan={columns.length} className="px-4 py-2 text-center">
+              <td colSpan={columns.length} className="px-4 py-2">
                 No data available
               </td>
             </tr>
           ) : (
             data.map((row, rowIndex) => (
-              <tr key={rowIndex}>
+              <tr key={rowIndex} className="hover:bg-gray-100 text-center">
                 {columns.map((column, colIndex) => (
-                  <td key={colIndex} className="px-4 py-2 border-b hover:bg-slate-200">
-                    {row[column] || "N/A"}
+                  <td key={colIndex} className="px-4 py-2 border-b border-x">
+                    {column.key === "image" ? (
+                      <img
+                        src={row[column.key]}
+                        alt="product"
+                        className="w-10 h-10 object-cover m-auto"
+                      />
+                    ) : column.key === "action" ? (
+                      <button className="text-blue-500 underline">
+                        ویرایش
+                      </button>
+                    ) : (
+                      row[column.key]
+                    )}
                   </td>
                 ))}
               </tr>
