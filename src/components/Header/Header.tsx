3582ab33e4d09684c435ed22702b96f112734f84
arsalan611 <amir.arsalan.poudat@gmail.com>
2025-04-13 20:03:54 +0330

    update: update header for new back-end

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,54 +1,55 @@
 import { useEffect, useState } from "react";
-import getAdminBio from "../../services/auth/getAdminBio/getAdminBio";
-import { BASE_URL } from "../../services/API/API";
 import { headerLocalization } from "../../constants/Localization/Localization";
 import Loader from "../Loading/Loading";
 import warning from "../../assets/images/warning.svg"
+import adminPhoto from "../../assets/images/adminPhoto.jpg"
 
 export default function Header() {
-  const [adminBio, setAdminBio] = useState<any>(null);
+  const [adminData, setAdminData] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    const fetchAdminBio = async () => {
-      try {
-        const data = await getAdminBio();
-        setAdminBio(data);
-      } catch (error) {
-        setError(true);
-        console.log(error)
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchAdminBio();
+try {
+  const getAdminData = JSON.parse(localStorage.getItem("adminData") || "{}");
+  setAdminData(getAdminData);
+  setLoading(false);
+} catch (error) {
+  setError(true);
+  console.log(error);
+} finally {
+  setLoading(false);
+}
   }, []);
 
-  console.log(adminBio);
+  console.log(adminData);
 
   if (loading)
     return (
-      <div className="p-5  bg-gray-100 shadow-sm w-full">
+      <div className="p-5 bg-gray-100 shadow-sm w-full ">
         <Loader />
       </div>
     );
   if (error) return <div className=" bg-gray-100 shadow-sm w-full">
-    <img src={warning} alt="warning" className="w-32 py-2" />
-    <p>{headerLocalization.warning}</p>
+    <img src={warning} alt="warning" className="w-32 py-2 m-auto" />
+    <p className="text-center">{headerLocalization.warning}</p>
   </div>;
 
   return (
-    <div dir="ltr" className="p-5 flex gap-2 self-end bg-gray-100 shadow-sm w-full">
-      <div className="w-14 border border-black rounded-full overflow-hidden">
+    <div
+      dir="ltr"
+      className="p-5 flex gap-2 self-end bg-gray-100 shadow-sm w-full"
+    >
+      <div className="w-14 h-14 border border-black rounded-full overflow-hidden">
         <img
-          src={BASE_URL + adminBio.image || "https://via.placeholder.com/150"}
+          src={
+            adminPhoto
+          }
           alt="profile picture"
         />
       </div>
       <div className="flex flex-col justify-center">
-        <p className="text-2xl">{adminBio.name}</p>
+        <p className="text-2xl">{adminData.username}</p>
         <p className="">{headerLocalization.admin}</p>
       </div>
     </div>
